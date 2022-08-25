233c7247bd604451e7c0967df67a330a7b5a5b4e
shai9 <dennis.shakava@student.moringaschool.com>
2022-08-25 16:31:54 +0300

    commit tree c7f012b2a2b0ff94409889410aea52b7c81e785b

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
 
   attr_accessor :name, :album
@@ -6,5 +7,24 @@ class Song
     @name = name
     @album = album
   end
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+
+  end
 end
